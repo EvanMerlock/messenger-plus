016aa43410c64e22101d09f0a5ef2cff186763e7
Evan Merlock <vnmrlck@gmail.com>
2017-04-28 13:03:12 -0500

    Removed extra tests that were migrated to documentation

--- src/stream/mod.rs
+++ src/stream/mod.rs
@@ -5,32 +5,6 @@ mod tests {
 
     use super::*;
 
-    #[test]
-    fn vec_slice_location() {
-        let mut vec: Vec<u8> = Vec::new();
-        vec.push(2);
-        vec.push(13);
-        vec.push(17);
-        vec.push(18);
-
-        let slice: &[u8] = &[2, 13];
-
-        assert_eq!(read_stream::find_where_slice_intersects(&vec, slice), Some(2));
-    }
-
-    #[test]
-    fn vec_slice_no_loc() {
-        let mut vec: Vec<u8> = Vec::new();
-        vec.push(2);
-        vec.push(13);
-        vec.push(17);
-        vec.push(18);
-
-        let slice: &[u8] = &[2, 2];
-
-        assert_eq!(read_stream::find_where_slice_intersects(&vec, slice), None);
-    }
-
         #[test]
     fn vec_slice_begin_location() {
         let mut vec: Vec<u8> = Vec::new();
